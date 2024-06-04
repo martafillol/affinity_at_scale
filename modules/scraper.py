91a138dde644b7f9abbaaa5467a77603c6e0e7ee
pablo lizardi <pabloagra3018@gmail.com>
2024-06-04 13:06:40 +0200

    scrapper working  with multiple url

--- modules/scraper.py
+++ modules/scraper.py
@@ -1,10 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import spacy
+from spacymoji import Emoji
 
 url = 'https://medium.com/@getgoing.ca/the-ultimate-guide-to-choosing-the-perfect-car-19747eec95e'
 
-def scraper(url):
+nlp = spacy.load("en_core_web_sm")
+nlp.add_pipe("emoji", first=True)
+
+
+def scrape(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
     scraped_data = []
@@ -15,5 +21,26 @@ def scraper(url):
         scraped_data.extend(texts)
 
     scraped_data = pd.Series(scraped_data)
-
     return scraped_data
+
+scrape_data = scrape(url)
+
+def text_cleaner(text):
+    serie_joined = ' '.join(text)
+    doc = nlp(serie_joined)
+    clean_words = []
+    for each in doc:
+        if each._.is_emoji or each.is_digit or each.like_url or each.like_email or each.is_punct:
+            continue
+        else:
+            clean_words.append(each)
+
+    return clean_words
+#Use scraper for multiple websites:
+def scraper(lst):
+    list = []
+    for l in lst:
+        scr = scrape(l)
+        clt = text_cleaner(scr)
+        list.extend(clt)
+    return list
